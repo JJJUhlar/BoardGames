ad9cc0e7a529659afa0ea345bcebd65a22255942
JJJUhlar <jjuhlar@gmail.com>
2023-02-20 14:29:37 +0000

    made some tests for get categories endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,66 @@
 const app = require('../app')
+const db = require('../db/connection')
+const request = require('supertest')
+const seed = require('../db/seeds/seed')
+const data = require('../db/data/test-data')
+
+
+// supertest re-seed database between tests
+beforeEach(()=>{
+    return seed(data)
+})
+
+// stop jest from hanging
+afterAll(()=>{
+    db.end()
+})
+
 
 describe('appTests', () => {
     describe('GET: /api/categories', () => {
-        test.todo('endpoint has 200 status')
-        test.todo('returns an array of category objects')
-        test.todo('category objects have slug, description properties')
-        test.todo('handles errors')
+        console.log(">>> /api/categories tests <<<")
+        
+        test('endpoint has 200 status', () => {
+            request(app)
+                .get('/api/categories')
+                .expect(200)
+        })
+        test('returns an array of category objects', () => {
+            request(app)
+                .get('/api/categories')
+                .expect(200)
+                .then(({body}) => {
+                    const {categories} = body;
+
+                    expect(Array.isArray(categories)).toBe(true)
+                })
+        })
+        test('category objects have slug, description properties', () => {
+            request(app)
+                .get('/api/categories')
+                .expect(200)
+                .then(({body}) => {
+                    const {categories} = body;
+                    categories.forEach((category)=>{
+                        expect(Object.keys(category).length).toBe(2)
+
+                        expect(category).toHaveProperty('slug', expect.any(String))
+                        expect(category).toHaveProperty('description', expect.any(String))
+                    })
+                })
+        })
+        console.log("<<< end of test block >>>")
+    })
+
+    describe('errors', () => {
+        test('responds 404 to non existent endpoint', () => {
+            request(app)
+                .get('/api/notanexistingendpoint')
+                .expect(404)
+        })
+        test('repsonds with an appropriate error message', () => {
+            request(app)
+                
+        })
     })
 })
--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require('express')
+const db = require('./db/connection')
+// require controllers
 
 
 const app = express()
+// add app.use json here
 
-
-app.get("/api/categories", (request, respsonse) => {
+app.get("/api/categories", (request, response, next) => {
+    return db.query('SELECT * FROM categories')
+        .then(({rows}) => {
+            console.log(rows)
+            return rows
+        })
+        .then((categories) => {
+            response.status(200).send({"categories":categories})
+        })
+        .catch((error)=>{
+            next(error)
+        })
 
 })
 
+// end points here to come
+
+
+// error handlers
+app.use((err,request,response,next) => {
+    console.log("error >>>", err)
+    next(err)
+})
+
+
+module.exports = app;
